b791d3308e6e9629a568d7f7989f3199019e85ee
ekahendar <hendar1994@gmail.com>
2024-07-14 10:54:19 +0700

    first commit

---
+++ djangovanilla/vanilla/vanilla/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls import include, path
+from filemanager.views import filemanager
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', filemanager),
+]
